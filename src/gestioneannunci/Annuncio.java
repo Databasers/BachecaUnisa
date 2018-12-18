bac75c39700afb40c8a2e7fab5cb7a1615fadb94
Federico De Mattia <federicodemattia98@gmail.com>
2018-12-18 14:11:31 +0100

    prova commento

--- src/gestioneannunci/Annuncio.java
+++ src/gestioneannunci/Annuncio.java
@@ -1,5 +1,12 @@
 package gestioneannunci;
 
+/**
+ * Un oggetto <code>Annuncio<\code> rappresenta un annuncio creato da un utente. Ogni annuncio ha un
+ * una tipologia (gruppo di studio, tutorato), un titolo, una descrizione facoltativa ed un dipartimento
+ * da selezionare
+ *
+ */
+
 public class Annuncio {
 
 }
