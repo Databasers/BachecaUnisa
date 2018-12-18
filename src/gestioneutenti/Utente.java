d5116178e2090a6bc5e56d2b6ce52cc9346965a4
Federico De Mattia <federicodemattia98@gmail.com>
2018-12-18 14:39:46 +0100

    commenti classi

--- src/gestioneutenti/Utente.java
+++ src/gestioneutenti/Utente.java
@@ -1,5 +1,11 @@
 package gestioneutenti;
 
+/**
+ * Un oggetto <code>Utente</code> rappresenta un utente. Ogni utente ha un un nome, un cognome,
+ * un sesso, un username, una password, una descrizione ed un numero annunci
+ *
+ */
+
 public class Utente {
 
 }
