46e91839c8adcf02f98b40d94458d0c5c5e7fb57
Federico De Mattia <federicodemattia98@gmail.com>
2018-12-19 16:33:36 +0100

    Modifica Recensione

--- src/gestionerecensioni/Recensione.java
+++ src/gestionerecensioni/Recensione.java
@@ -8,5 +8,30 @@ package gestionerecensioni;
  */
 
 public class Recensione {
+	
+	
+	public Recensione(int valutazione, String descrizione) {
+		this.valutazione = valutazione;
+		this.descrizione = descrizione;
+	}
+	
+	
+	
+	public int getValutazione() {
+		return valutazione;
+	}
+	public void setValutazione(int valutazione) {
+		this.valutazione = valutazione;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
 
+
+
+	private int valutazione;
+	private String descrizione;
 }
