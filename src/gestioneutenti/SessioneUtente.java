2659a211abccbbead10953b6c1474252fe6b078a
Federico De Mattia <federicodemattia98@gmail.com>
2019-01-26 15:15:48 +0100

    sessione utente

--- src/gestioneutenti/SessioneUtente.java
+++ src/gestioneutenti/SessioneUtente.java
@@ -2,44 +2,51 @@ package gestioneutenti;
 
 import java.io.Serializable;
 
-public class SessioneUtente implements Serializable{
+public class SessioneUtente implements Serializable {
 
+  private static final long serialVersionUID = 1L;
+  
+  
+  public SessioneUtente(Utente u) {
+    username = u.getUsername();
+    nome = u.getNome();
+    cognome = u.getCognome();
+  }
+  
+  public SessioneUtente(Utente u, String ruolo) {
+    username = u.getUsername();
+    nome = u.getNome();
+    cognome = u.getCognome();
+    this.ruolo = ruolo;
+  }
 
+  public String getUsername() {
+    return username;
+  }
 
-	
-	private static final long serialVersionUID = 1L;
-	public SessioneUtente(Utente u) {
-		username=u.getUsername();
-		nome=u.getNome();
-		cognome=u.getCognome();
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-	public String getUsername() {
-		return username;
-	}
+  public String getNome() {
+    return nome;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-	public String getNome() {
-		return nome;
-	}
+  public String getCognome() {
+    return cognome;
+  }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+  public void setCognome(String cognome) {
+    this.cognome = cognome;
+  }
 
-	public String getCognome() {
-		return cognome;
-	}
-
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
-
-	 private String username;
-   private String nome;
-   private String cognome;
+  private String username;
+  private String nome;
+  private String cognome;
+  private String ruolo;
 
 }
